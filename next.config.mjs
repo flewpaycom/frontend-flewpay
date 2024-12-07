f34cb229d3536f78d250f108a1c3b375a6981ccf
Leonardo Ramirez <juanlov4321@gmail.com>
2024-12-07 01:28:45 -0500

    ✨ update webpack configuration to add external dependencies for pino-pretty, lokijs, and encoding

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    webpack: config => {
+        config.externals.push('pino-pretty', 'lokijs', 'encoding')
+        return config
+    }
 };
- 
+
 export default withNextIntl(nextConfig);
