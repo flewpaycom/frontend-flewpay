243e1d5a02487263441386690daeb853393509f1
Leonardo Ramirez <juanlov4321@gmail.com>
2024-12-08 18:27:44 -0500

    ✨ refactor SwitchLang component to simplify structure, improve styling, and enhance button functionality

--- src/components/SwitchLang/index.tsx
+++ src/components/SwitchLang/index.tsx
@@ -20,12 +20,12 @@ const SwitchLang = () => {
 	};
 
 	return (
-		<div className="relative">
-			<button onClick={() => setIsOpen(!isOpen)} className="absolute top-0 right-0 m-4 flex items-center space-x-2 bg-white text-gray-600 rounded-full p-2 shadow-md hover:shadow-lg transition-shadow" aria-label="Change Language">
+		<div>
+			<button onClick={() => setIsOpen(!isOpen)} className="m-4 flex items-center space-x-2 bg-white text-gray-600 rounded-full p-2 shadow-md hover:shadow-lg transition-shadow" aria-label="Change Language">
 				{languages.find((lang) => lang.code === currentLocale)?.icon}
 			</button>
 			{isOpen && (
-				<div className="absolute top-12 right-4 w-40 rounded-lg bg-white shadow-lg ring-1 ring-gray-200 z-10">
+				<div className="absolute w-40 rounded-lg bg-white shadow-lg ring-1 ring-gray-200 z-10">
 					{languages.map((lang) => (
 						<div key={lang.code} onClick={() => handleChangeLang(lang.code)} className="flex items-center cursor-pointer px-4 py-2 hover:bg-gray-100 transition-colors rounded">
 							<span className="text-xl">{lang.icon}</span>
