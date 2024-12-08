2e83ea73016ab195f65cd37ee0c7466acd1efa19
Leonardo Ramirez <juanlov4321@gmail.com>
2024-12-08 00:25:07 -0500

    ✨ enhance ContextProvider with cookie-based initial state and default network configuration

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,34 +1,41 @@
 "use client";
 
-import React, { ReactNode } from "react";
-import { createAppKit } from "@reown/appkit/react";
 import { wagmiAdapter, projectId, siweConfig, metadata, chains } from "../config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { State, WagmiProvider } from "wagmi";
+import { createAppKit } from "@reown/appkit/react";
+import { arbitrum } from "@reown/appkit/networks";
+import React, { type ReactNode } from "react";
+import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
 
-// Setup queryClient
+// Set up queryClient
 const queryClient = new QueryClient();
 
-if (!projectId) throw new Error("Project ID is not defined");
+if (!projectId) {
+	throw new Error("Project ID is not defined");
+}
 
-// Create modal
-createAppKit({
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const modal = createAppKit({
 	adapters: [wagmiAdapter],
-	networks: chains,
 	projectId,
-	siweConfig,
-	metadata,
+
+	networks: chains,
+	defaultNetwork: arbitrum,
+	siweConfig: siweConfig,
+	metadata: metadata,
 	features: {
-		email: true,
-		socials: ["google", "x", "github", "discord", "apple", "facebook", "farcaster"],
-		emailShowWallets: true,
+		onramp: true,
 	},
 });
 
-export default function AppKitProvider({ children, initialState }: { children: ReactNode; initialState?: State }) {
+function ContextProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
+	const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies);
+
 	return (
-		<WagmiProvider config={wagmiAdapter.wagmiConfig} initialState={initialState}>
+		<WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
 			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 		</WagmiProvider>
 	);
 }
+
+export default ContextProvider;
