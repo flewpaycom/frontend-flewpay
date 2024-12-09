760446160fdd01d5c5d0bfe044f747a1698d6aae
Leonardo Ramirez <juanlov4321@gmail.com>
2024-12-08 20:16:24 -0500

    ✨ update SectionAction component to integrate translations for title, subtitle, and button texts

--- src/components/landing/SectionAction.tsx
+++ src/components/landing/SectionAction.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { useTranslations } from "next-intl";
 
 const SectionAction = () => {
+	const t = useTranslations("landing.SectionAction");
 	return (
 		<section className="py-20 px-8 md:px-12" data-aos="fade-up">
 			<div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-between rounded-3xl shadow-lg bg-white">
 				<div className="text-dark-blue p-10 md:p-16 flex-1">
-					<h2 className="text-4xl font-bold mb-4 text-gray-800">Ready to level up your payment process?</h2>
-					<p className="text-gray-600 mb-8">Supports small businesses with simple invoicing, powerful integrations, and cash flow management tools.</p>
+					<h2 className="text-4xl font-bold mb-4 text-gray-800">{t("title")}</h2>
+					<p className="text-gray-600 mb-8">{t("subtitle")}</p>
 					<div className="flex items-center gap-4">
-						<button className="btn btn-primary rounded-badge font-medium py-3 px-6">Get Started Now</button>
-						<button className="btn btn-outline btn-secondary font-medium py-3 px-6 rounded-badge">Learn More</button>
+						<button className="btn btn-primary rounded-badge font-medium py-3 px-6">{t("button")}</button>
+						<button className="btn btn-outline btn-secondary font-medium py-3 px-6 rounded-badge">{t("othercalltoaction")}</button>
 					</div>
 				</div>
 				<div className="flex-1">
