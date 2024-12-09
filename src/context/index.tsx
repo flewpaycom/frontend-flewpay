6e60f225776a182039de22be4d58a07938b4a263
Leonardo Ramirez <juanlov4321@gmail.com>
2024-12-08 22:32:58 -0500

    ✨ add Orbit FlewPay network configuration to context and update networks array

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -3,7 +3,7 @@
 import { wagmiAdapter, projectId, siweConfig, metadata, chains } from "../config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createAppKit } from "@reown/appkit/react";
-import { arbitrum } from "@reown/appkit/networks";
+import { arbitrum, defineChain } from "@reown/appkit/networks";
 import React, { type ReactNode } from "react";
 import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
 
@@ -14,12 +14,31 @@ if (!projectId) {
 	throw new Error("Project ID is not defined");
 }
 
+const orbitNetwork = defineChain({
+	id: 28133214008,
+	caipNetworkId: "eip155:28133214008",
+	chainNamespace: "eip155",
+
+	name: "Orbit FlewPay",
+	nativeCurrency: {
+		decimals: 18,
+		name: "Ether",
+		symbol: "ETH",
+	},
+	rpcUrls: {
+		default: {
+			http: ["https://a877-152-201-123-55.ngrok-free.app"],
+		},
+	},
+	testnet: true,
+});
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const modal = createAppKit({
 	adapters: [wagmiAdapter],
 	projectId,
 
-	networks: chains,
+	networks: [orbitNetwork, ...chains],
 	defaultNetwork: arbitrum,
 	siweConfig: siweConfig,
 	metadata: metadata,
